027f9124a5fc3c529708ae06b5fae24425367e2e
KevalP24 <kevalrp2004@gmail.com>
2024-09-03 12:26:04 +0530

    commit top car

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,29 @@ navToggler.addEventListener("click", function () {
   navbar.classList.toggle("active");
   this.classList.toggle("active");
 });
+
+
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 30, 
+  loop: false, // Disable loop if it's causing problems
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
